791d6052acd11b9d8c97085cff21dfb6f06e5824
Chandrima Sen <chandrimasen17@gmail.com>
2023-09-07 17:01:11 -0400

    modif

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,7 +1,7 @@
 from palindrome import is_palindrome
 
 def test_palindrome_true():
-    assert is_palindrome('asghjyt')
+    assert is_palindrome('racecar')
 
 def test_palindrome_false():
     assert not is_palindrome('asertd')
